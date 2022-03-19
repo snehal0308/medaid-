9fa37f6e969d8253788afd4ff24626850a7ffc2c
Snehal Adbol <smitaadbol01@gmail.com>
2022-03-19 23:52:47 +0530

    added voice call

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request , render_template, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse,Gather
 from os import path
 
 
@@ -12,13 +13,64 @@ def index():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
 
-    msg = request.form.get('Body').lower()
+    # Create reply
     resp = MessagingResponse()
 
 
-    if msg == 'hello': 
-        resp.message('hi!')
+    if '--Tasks'.lower() in msg:
+        resp.message(f'Hello!')
+
+
+
+    return str(resp)
+
+@app.route("/answer", methods=['GET', 'POST'])
+def answer_call():
+    """Respond to incoming phone calls with a brief message."""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Start our <Gather> verb
+    gather = Gather(num_digits=1, action='/gather')
+    gather.say('For med1, press 1. For med2, press 2.')
+    resp.append(gather)
+
+
+    # If the user doesn't select an option, redirect them into a loop
+    resp.redirect('/voice')
+
+    return str(resp)
+
+
+@app.route('/gather', methods=['GET', 'POST'])
+def gather():
+    """Processes results from the <Gather> prompt in /voice"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # If Twilio's request to our app included already gathered digits,
+    # process them
+    if 'Digits' in request.values:
+        # Get which digit the caller chose
+        choice = request.values['Digits']
+
+        # <Say> a different message depending on the caller's choice
+        if choice == '1':
+            resp.say('You selected medicine 1 . ')
+            return str(resp)
+        elif choice == '2':
+            resp.say('You selected medicine 2. ')
+            return str(resp)
+        else:
+            # If the caller didn't choose 1 or 2, apologize and ask them again
+            resp.say("Sorry, I don't understand that choice.")
+
+    # If the user didn't choose 1 or 2 (or anything), send them back to /voice
+    resp.redirect('/voice')
 
     return str(resp)
 
